c93a343935e4455b6de593b4b0e5e7f3be71a298
coolsaucesam <143228294+coolsaucesam@users.noreply.github.com>
2025-02-24 10:14:18 -0500

    Add files via upload

---
+++ diffraction_analysis_final.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+from scipy.optimize import curve_fit
+
+#import and initialize data
+data = pd.read_csv("single_slit_data.csv", header=0)
+
+# multiplied to get in meters
+pos_80 = np.array(data["80mim_pos"])*10**(-3)
+volts_80 = np.array(data["80_volts"])
+
+pos_40 = np.array(data["40_pos"])*10**(-3)
+volts_40 = np.array(data["40_volts"])
+
+X = np.linspace(0, 0.046, 1000)
+Y = np.linspace(0, 0.092, 1000)
+
+# initialize constants
+wavelength = 594*10**(-9)
+k = (2*np.pi)/wavelength #wave number
+a80 = 80*10**(-6) # nominal slit width
+a40 = 40*10**(-6) # nominal width
+L = 1.04  # measured @ 104cm +- 1mm - separation between slit and diffraction pattern
+
+# lets create a combined constant q_0 that is 2L/ka 
+q0_80 = (2*L)/(k * a80) # initial guess value
+q0_40 = (2*L)/(k * a40)
+
+
+I_0 = 3*10**(-3) #maximum intensity 
+I0_40 = 3*10**(-2)
+
+
+# potentially lump L,k, and a into one param
+# fringe model for a single slit
+# params are x_0: center fringe location
+# q: defined above 
+# I: maximum intensity. 
+def fringe_model(x, x_0, q, I):
+    return I * (q/(x-x_0))**2 * (np.sin((x-x_0)/q))**2
+
+# lets calculate the uncertainty in a:
+# deltax is the uncertainty in L, and delta y is the uncertainty in q
+def calculateUncertainties(deltax, deltay, q):
+    return np.sqrt((2/(k*q)*deltax)**2+
+                    ((2*L)/(k*q**2)*deltay)**2)
+
+# Now to determine appropriate initial values to create our best fit model
+initialGuesses80 = np.array([.023, q0_80, I_0])
+initialGuesses40 = np.array([.046, q0_40, I0_40])
+# testing out guesses with
+# calculatedGuesses80 = fringe_model(pos_80, 23, q_0, I_0)
+# calculatedGuess40 = fringe_model(Y, 46, q0_40, I0_40)
+# performing the fit
+params80Micrometers, cov80Micrometers = curve_fit(fringe_model, pos_80, volts_80, p0=initialGuesses80)
+params40Micrometers, cov40Micrometers = curve_fit(fringe_model, pos_40, volts_40, p0=initialGuesses40)
+
+print('---------------------Uncertainty for 80 micrometer slit-------------------\n')
+uncertainties80 = np.sqrt(np.diag(cov80Micrometers))
+print("x_0:", params80Micrometers[0])
+print("q:", params80Micrometers[1])
+print()
+print("a:", (2*L)/(abs(params80Micrometers[1])*k)*10**6)
+print("The uncertainties in fit are:", uncertainties80)
+print("absolute: +-", calculateUncertainties(.001, uncertainties80[1], params80Micrometers[1])*10**6, "μm , % uncertainty:", calculateUncertainties(.001,uncertainties80[1], params80Micrometers[1])/((2*L)/(abs(params80Micrometers[1])*k)))
+
+print('\n----------------Uncertainty for 40 micrometer slit-----------------\n')
+uncertainties40 = np.sqrt(np.diag(cov40Micrometers))
+print("x_0:", params40Micrometers[0])
+print("q:", params40Micrometers[1])
+
+print("a:", (2*L)/(abs(params40Micrometers[1])*k)*10**6)
+print("The uncertainties in fit are:", uncertainties40)
+print("absolute:", calculateUncertainties(.001, 7.35714755e-05, params40Micrometers[1]), "% uncertainty:", calculateUncertainties(.001, 7.35714755e-05, params40Micrometers[1])/((2*L)/(abs(params40Micrometers[1])*k)))
+
+
+#calculating residuals:
+residuals80 = []
+residuals40 = []
+for x in range(len(volts_80)):
+    residuals80.append(volts_80[x] - fringe_model(x*10**(-3), params80Micrometers[0], params80Micrometers[1], params80Micrometers[2]))
+
+for y in range(len(volts_40)):
+    residuals40.append(volts_40[y] - fringe_model(y*2*10**(-3), params40Micrometers[0], params40Micrometers[1], params40Micrometers[2]))
+
+fig, (ax1, ax2) = plt.subplots(1,2, layout='constrained', figsize=(12,5))
+
+
+#plotting raw data for 80micrometer slit
+ax1.set_title('80μm Data')
+ax1.plot(pos_80, volts_80, marker='o', ls='None', color='black', label="data", markersize=5)
+ax1.plot(pos_80, fringe_model(pos_80, params80Micrometers[0], params80Micrometers[1], params80Micrometers[2]), color='black', label="best fit curve", linewidth=1.5, linestyle='dashed')
+# plt.plot(pos_80, residuals80, linestyle='dashed', label='residuals', color='black')
+ax1.set_xticks([ 0.003, .013, .023655, .034, .044], [ '-20', '-10' ,'0', '10','20'])
+# plt.axvline(params80Micrometers[0], color='black', linestyle='dotted')
+ax1.set_ylabel(" Volts [V]")
+ax1.set_xlabel(" Position [mm]")
+#plt.legend()
+ax1.grid()
+
+#plotting data from 40μm
+ax2.set_title('40μm Data')
+ax2.plot(pos_40, volts_40, 'o', color='blue', label='data', markersize=5)
+ax2.plot(pos_40, fringe_model(pos_40, params40Micrometers[0], params40Micrometers[1], params40Micrometers[2]), color='b', linestyle='dashed', linewidth=1.5,  label='best fit curve')
+# plt.plot(pos_40, residuals40, linestyle='dashed', label='residuals', color='blue')
+ax2.set_xticks([0.04685-.04, 0.04685-.02, .04685,0.04685+.02, 0.04685+.04], ['-40', '-20' ,'0', '20','40'])
+ax2.set_ylabel("Volts [V]")
+ax2.set_xlabel("Position [mm]")
+#plt.legend()
+ax2.grid()
+plt.savefig('data_plot.png', dpi=250)
+  
+# plot of the residuals  
+plt.figure()
+plt.title('residuals')
+plt.plot(pos_80, residuals80, label='80 micrometer', color='black')
+plt.plot(pos_40, residuals40, label='40 micrometer', color='blue')
+plt.savefig("residuals.png", dpi=1000)
+# plt.figure()
+# plt.plot(Y, fringe_model(Y, 46, q0_40, I0_40))
---
+++ mech_c_analysis.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit  
+import matplotlib.pyplot as plt 
+
+# Read in data
+data = pd.read_csv("data_2.csv", header=0)
+
+rev = data["speed"]
+deflection = data["deflection"] * 10**(-3) # factor of 10e-3 to convert mm to m
+
+lit_c = 299792458 # [m/s] speed of light in literature.
+
+# Constants and Uncertainties
+A = 0.267  # distance between L2 and L1 in mm
+deltaA = 0.5 * 10**(-3)  # +- 0.5mm
+B = 0.483  # distance between L2 and Mr in mm
+deltaB = 0.5 * 10**(-3)  # +- 0.5 mm
+D = 9.94  # distance between Mr and Mf
+deltaD = 0.1  # uncertainty in D
+deltaSPrime = 0.005 * 10**(-3)  # uncertainty in s'
+deltaOmega = 1  # uncertainty in omega
+
+# constant combining all constants in equation for c
+k = (8 * np.pi * A * D**2) / (B + D)
+
+# Calculate the uncertainty in k
+uncertainty_in_k = np.sqrt(
+    ( (8*np.pi * D**2) / (B + D) * deltaA) ** 2 +
+    ( (8*np.pi * A * D**2) / (B+D**2) * deltaB) ** 2 +
+    ( (8*np.pi * A * D * (2*B + D))/((B+D)**2) * deltaD) ** 2)
+
+# Equation from lab
+def return_c(rev, deflection):
+    return (8 * np.pi * A * D**2 * rev) / ((D + B) * deflection)
+
+# Linear model
+def model(x, H, J):
+    return H * x + J
+
+# Calculate 'c' values and linear fit data
+# Clist = []
+# for h in range(len(deflection)):
+#     Clist.append(return_c(rev[h], deflection[h]))
+
+
+# Now, implement curve fitting with uncertainty
+initial_guesses = np.array([0, 0])  # Speed of light guess and offset
+
+# Uncertainty vector
+# uncertainties = [calculateUncertainty(rev[i], deflection[i]) for i in range(len(rev))]
+
+# Perform curve fitting with uncertainties (use 'sigma' to provide weights for the fit)
+params, cov = curve_fit(model, rev, deflection, p0=initial_guesses)
+
+# calculating the uncertainty in c using k/slope
+print(np.sqrt(np.diag(cov)))
+uncertainty_in_c = np.sqrt(
+    (k/((params[0])**2) *(np.sqrt(np.diag(cov)))[0])**2 +
+    (1/(params[0]*uncertainty_in_k)**2)
+    )
+    
+print(uncertainty_in_c)
+exp_c = k/params[0]
+# Print results and analyze
+print(f"Fitted Parameters: H = {params[0]:.3e}, J = {params[1]:.3e}")
+print(f"Speed of light from fit: {k / params[0]:.3e} plus/minus {uncertainty_in_c:.2e} m/s")
+print('percent error in c', round(uncertainty_in_c/exp_c, 5))
+
+# Plot the results with uncertainties
+X = np.linspace(-1000, 1000, 100)
+fig, ax = plt.subplots()
+# Plot the linear fit
+ax.scatter(rev, deflection, color='blue')
+ax.plot(X, model(X, params[0], params[1]), label="Linear Fit", color='orange')
+# Labels and title
+ax.set_xlabel("Rotation Speed (rev/s)")
+ax.set_ylabel("Deflection (m)")
+ax.set_title("Linear Fit to Speed of Light Experiment")
+ax.grid()
+# Show plot
+plt.savefig('mech_c_fit.png', dpi=200)
+plt.show()
